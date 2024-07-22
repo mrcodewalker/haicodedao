2bba57202fc62ba50c0040d6ae8f83645284baee
mrcodewalker <mr.codewalker@gmail.com>
2024-07-22 22:14:58 +0700

    KMA Schedule

--- src/main/java/com/example/codewalker/kma/Main.java
+++ src/main/java/com/example/codewalker/kma/Main.java
@@ -1,6 +1,18 @@
 package com.example.codewalker.kma;
 
+import io.jsonwebtoken.io.Encoders;
+
+import java.security.SecureRandom;
+
 public class Main {
     public static void main(String[] args) throws Exception {
+        System.out.println(new Main().generateSecretKey());
+    }
+    private String generateSecretKey(){
+        SecureRandom random = new SecureRandom();
+        byte[] keyBytes = new byte[32];
+        random.nextBytes(keyBytes);
+        String secretKey = Encoders.BASE64.encode(keyBytes);
+        return secretKey;
     }
 }
