1d05673a32a4d68f6d21ea1ade75e0fc846e4119
mrcodewalker <mr.codewalker@gmail.com>
2024-09-04 14:57:42 +0700

    Stop tracking SYSTEM32.env

--- src/main/java/com/example/codewalker/kma/Application.java
+++ src/main/java/com/example/codewalker/kma/Application.java
@@ -11,21 +11,20 @@ public class Application {
 		// Tải biến môi trường từ file .env
 		Dotenv dotenv = Dotenv.configure()
 				.directory("/etc/secrets")
-				.filename("SYSTEM32")
+				.filename("SYSTEM32.env")
 				.load();
 
-		System.setProperty("GOOGLE_CLIENT_ID", System.getenv("GOOGLE_CLIENT_ID"));
-		System.setProperty("GOOGLE_CLIENT_SECRET", System.getenv("GOOGLE_CLIENT_SECRET"));
-		System.setProperty("FACEBOOK_CLIENT_ID", System.getenv("FACEBOOK_CLIENT_ID"));
-		System.setProperty("FACEBOOK_CLIENT_SECRET", System.getenv("FACEBOOK_CLIENT_SECRET"));
-		System.setProperty("GITHUB_CLIENT_ID", System.getenv("GITHUB_CLIENT_ID"));
-		System.setProperty("GITHUB_CLIENT_SECRET", System.getenv("GITHUB_CLIENT_SECRET"));
-		System.setProperty("SPRING_DATASOURCE_URL", System.getenv("SPRING_DATASOURCE_URL"));
-		System.setProperty("SPRING_DATASOURCE_USERNAME", System.getenv("SPRING_DATASOURCE_USERNAME"));
-		System.setProperty("SPRING_DATASOURCE_PASSWORD", System.getenv("SPRING_DATASOURCE_PASSWORD"));
-		System.setProperty("JWT_SECRET", System.getenv("JWT_SECRET"));
-		System.setProperty("JWT_EXPIRATION", System.getenv("JWT_EXPIRATION"));
-
+		System.setProperty("GOOGLE_CLIENT_ID", dotenv.get("GOOGLE_CLIENT_ID"));
+		System.setProperty("GOOGLE_CLIENT_SECRET", dotenv.get("GOOGLE_CLIENT_SECRET"));
+		System.setProperty("FACEBOOK_CLIENT_ID", dotenv.get("FACEBOOK_CLIENT_ID"));
+		System.setProperty("FACEBOOK_CLIENT_SECRET", dotenv.get("FACEBOOK_CLIENT_SECRET"));
+		System.setProperty("GITHUB_CLIENT_ID", dotenv.get("GITHUB_CLIENT_ID"));
+		System.setProperty("GITHUB_CLIENT_SECRET", dotenv.get("GITHUB_CLIENT_SECRET"));
+		System.setProperty("SPRING_DATASOURCE_URL", dotenv.get("SPRING_DATASOURCE_URL"));
+		System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv.get("SPRING_DATASOURCE_USERNAME"));
+		System.setProperty("SPRING_DATASOURCE_PASSWORD", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
+		System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
+		System.setProperty("JWT_EXPIRATION", dotenv.get("JWT_EXPIRATION"));
 
 		// Khởi động ứng dụng Spring Boot
 		SpringApplication.run(Application.class, args);
