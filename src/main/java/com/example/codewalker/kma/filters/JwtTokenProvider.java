6c1f02e39f12c2b1efec1c32b619ddc46a371d7e
mrcodewalker <mr.codewalker@gmail.com>
2024-09-10 15:29:26 +0700

    Small changes

--- src/main/java/com/example/codewalker/kma/filters/JwtTokenProvider.java
+++ src/main/java/com/example/codewalker/kma/filters/JwtTokenProvider.java
@@ -5,11 +5,14 @@ import com.example.codewalker.kma.models.User;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
@@ -29,71 +32,71 @@ public class JwtTokenProvider {
     @Value("${jwt.expiration}")
     private long jwtExpiration;
 
-    // Generate token
+    // Tạo token
     public String generateToken(User user) throws InvalidParamException {
         Map<String, Object> claims = new HashMap<>();
+//        this.generateSecretKey();
         claims.put("username", user.getUsername());
         claims.put("userId", user.getUserId());
         Date now = new Date();
         Date expiryDate = new Date(now.getTime() + jwtExpiration);
 
-        try {
-            return Jwts.builder()
-                    .setClaims(claims)
-                    .setIssuedAt(now)
+        try{
+            String token = Jwts.builder()
+                    .setClaims(claims) // how to extract claims from this ?
+                    .setIssuedAt(now) // Thêm thời gian phát hành
                     .setSubject(user.getUsername())
                     .setExpiration(expiryDate)
-                    .signWith(getSigninKey(), SignatureAlgorithm.HS256)
+                    .signWith(SignatureAlgorithm.HS256, getSigninKey()) // Sử dụng phương thức signWith mới
                     .compact();
-        } catch (Exception e) {
-            throw new InvalidParamException("Cannot create JWT token, error: " + e.getMessage());
+            return token;
+        } catch (Exception e){
+            throw new InvalidParamException("Can not create jwt token, error: "+e.getMessage());
         }
     }
 
     public boolean validateToken(String token) {
         try {
-            Jwts.parserBuilder().setSigningKey(getSigninKey()).build().parseClaimsJws(token);
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token);
             return true;
         } catch (Exception e) {
             return false;
         }
     }
-
-    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
-        final Claims claims = extractAllClaims(token);
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver){
+        final Claims claims = this.extractAllClaims(token);
         return claimsResolver.apply(claims);
     }
-
     public String getUsernameFromToken(String token) {
-        Claims claims = extractAllClaims(token);
+        Claims claims = Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody();
         return claims.getSubject();
     }
-
-    public boolean checkExtractToken(String token, String username) {
-        return getUsernameFromToken(token).equalsIgnoreCase(username);
+    public boolean checkExtractToken(String token, String username){
+        if (this.getUsernameFromToken(token).equalsIgnoreCase(username)){
+            return true;
+        }
+        return false;
     }
-
+    // Lấy thông tin từ token
     public Claims getClaims(String token) {
-        return extractAllClaims(token);
+        return Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody();
     }
 
-    public String extractUserName(String token) {
-        return extractClaim(token, Claims::getSubject);
+    // Lấy tên người dùng từ token
+    public String extractUserName(String token){
+        return this.extractClaim(token, Claims::getSubject);
     }
 
     private Key getSigninKey() {
         byte[] keyBytes = Base64.getDecoder().decode(jwtSecret);
         return Keys.hmacShaKeyFor(keyBytes);
     }
-
     private Claims extractAllClaims(String token) {
-        return Jwts.parserBuilder()
+        return Jwts.parser()
                 .setSigningKey(getSigninKey())
-                .build()
                 .parseClaimsJws(token)
                 .getBody();
     }
-
     public String resolveToken(HttpServletRequest request) {
         String bearerToken = request.getHeader("Authorization");
         if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
@@ -101,9 +104,9 @@ public class JwtTokenProvider {
         }
         return null;
     }
-
-    public boolean isTokenExpired(String token) {
-        Date expirationDate = extractClaim(token, Claims::getExpiration);
+    public boolean isTokenExpired(String token){
+        Date expirationDate = this.extractClaim(token, Claims::getExpiration);
         return expirationDate.before(new Date());
+
     }
 }
