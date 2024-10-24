ab25432256c00f5b1af3ecd4b78657cfd6822cf9
mrcodewalker <mr.codewalker@gmail.com>
2024-10-25 00:50:38 +0700

    KMA Legend

--- src/main/java/com/example/codewalker/kma/Application.java
+++ src/main/java/com/example/codewalker/kma/Application.java
@@ -19,20 +19,20 @@ public class Application {
 	public static void main(String[] args) {
 		// Tải biến môi trường từ file .env
 		Dotenv dotenv = Dotenv.configure()
-				.directory(".")
-				.filename("SYSTEM32.env") // Đảm bảo tệp .env đúng tên
+				.directory("C:\\Users\\ADMIN\\MyWebsite\\codewalker.kma\\codewalker.kma") // Chỉ định thư mục chứa file .env
+				.filename("SYSTEM32.env") // Tên file .env
 				.load();
 
 		// Thiết lập các thuộc tính hệ thống từ các biến môi trường
-		System.setProperty("server.port", dotenv.get("SERVER_PORT", "8080"));
+//		System.setProperty("server.port", dotenv.get("SERVER_PORT", "8080"));
 		System.setProperty("spring.datasource.url", dotenv.get("SPRING_DATASOURCE_URL", "jdbc:mysql://localhost:4306/clone"));
 		System.setProperty("spring.datasource.username", dotenv.get("SPRING_DATASOURCE_USERNAME", "root"));
 		System.setProperty("spring.datasource.password", dotenv.get("SPRING_DATASOURCE_PASSWORD", "123456789"));
-		System.setProperty("spring.datasource.driver-class-name", dotenv.get("SPRING_DATASOURCE_DRIVER_CLASS_NAME", "com.mysql.cj.jdbc.Driver"));
-		System.setProperty("spring.jpa.show-sql", dotenv.get("JPA_SHOW_SQL", "true"));
-		System.setProperty("spring.jpa.hibernate.ddl-auto", dotenv.get("JPA_HIBERNATE_DDL_AUTO", "none"));
-		System.setProperty("spring.jpa.properties.hibernate.dialect", dotenv.get("JPA_HIBERNATE_DIALECT", "org.hibernate.dialect.MySQL8Dialect"));
-		System.setProperty("spring.jpa.properties.hibernate.format_sql", dotenv.get("JPA_HIBERNATE_FORMAT_SQL", "true"));
+//		System.setProperty("spring.datasource.driver-class-name", dotenv.get("SPRING_DATASOURCE_DRIVER_CLASS_NAME", "com.mysql.cj.jdbc.Driver"));
+//		System.setProperty("spring.jpa.show-sql", dotenv.get("JPA_SHOW_SQL", "true"));
+//		System.setProperty("spring.jpa.hibernate.ddl-auto", dotenv.get("JPA_HIBERNATE_DDL_AUTO", "none"));
+//		System.setProperty("spring.jpa.properties.hibernate.dialect", dotenv.get("JPA_HIBERNATE_DIALECT", "org.hibernate.dialect.MySQL8Dialect"));
+//		System.setProperty("spring.jpa.properties.hibernate.format_sql", dotenv.get("JPA_HIBERNATE_FORMAT_SQL", "true"));
 		System.setProperty("api.prefix", dotenv.get("API_PREFIX", "api/v1"));
 		System.setProperty("jwt.secret", dotenv.get("JWT_SECRET", "Z54uiPhveohL/uORp8a8rHhu0qalR4Mj+aIOz5ZA5zY="));
 		System.setProperty("jwt.expiration", dotenv.get("JWT_EXPIRATION", "8640000"));
--- src/main/java/com/example/codewalker/kma/configurations/SecurityConfig.java
+++ src/main/java/com/example/codewalker/kma/configurations/SecurityConfig.java
@@ -7,6 +7,7 @@ import com.example.codewalker.kma.filters.JwtTokenProvider;
 import com.example.codewalker.kma.repositories.UserRepository;
 import com.example.codewalker.kma.services.EmailService;
 import com.example.codewalker.kma.services.FacebookService;
+import io.github.cdimascio.dotenv.Dotenv;
 import jakarta.servlet.Filter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
@@ -48,6 +49,10 @@ public class SecurityConfig{
 
     private String apiPrefix = "api/v1";
     private final UserRepository userRepository;
+    private Dotenv dotenv = Dotenv.configure()
+            .directory("C:\\Users\\ADMIN\\MyWebsite\\codewalker.kma\\codewalker.kma") // Chỉ định thư mục chứa file .env
+            .filename("SYSTEM32.env") // Tên file .env
+            .load();
     @Bean
     public UserDetailsService userDetailsService(){
         return username ->
@@ -55,6 +60,48 @@ public class SecurityConfig{
                         .orElseThrow(() ->
                                 new UsernameNotFoundException(
                                         "Can not find user with phoneNumber: "+username));
+    }
+
+    @Bean
+    public ClientRegistrationRepository clientRegistrationRepository() {
+        ClientRegistration googleClient = ClientRegistration.withRegistrationId("google")
+                .clientId(dotenv.get("GOOGLE_CLIENT_ID"))  // Đọc từ .env
+                .clientSecret(dotenv.get("GOOGLE_CLIENT_SECRET"))  // Đọc từ .env
+                .authorizationUri("https://accounts.google.com/o/oauth2/auth")
+                .tokenUri("https://oauth2.googleapis.com/token")
+                .userInfoUri("https://www.googleapis.com/oauth2/v3/userinfo")
+                .redirectUri("https://www.laptopaz.id.vn/login/oauth2/code/google")
+                .scope("openid", "profile", "email")
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .jwkSetUri("https://www.googleapis.com/oauth2/v3/certs")
+                .userNameAttributeName("sub")
+                .build();
+
+        ClientRegistration facebookClient = ClientRegistration.withRegistrationId("facebook")
+                .clientId(dotenv.get("FACEBOOK_CLIENT_ID"))  // Đọc từ .env
+                .clientSecret(dotenv.get("FACEBOOK_CLIENT_SECRET"))  // Đọc từ .env
+                .authorizationUri("https://www.facebook.com/v17.0/dialog/oauth")
+                .tokenUri("https://graph.facebook.com/v17.0/oauth/access_token")
+                .userInfoUri("https://graph.facebook.com/me?fields=id,name,email")
+                .redirectUri("https://www.laptopaz.id.vn/login/oauth2/code/facebook")
+                .scope("public_profile", "email")
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .userNameAttributeName("id")
+                .build();
+
+        ClientRegistration githubClient = ClientRegistration.withRegistrationId("github")
+                .clientId(dotenv.get("GITHUB_CLIENT_ID"))  // Đọc từ .env
+                .clientSecret(dotenv.get("GITHUB_CLIENT_SECRET"))  // Đọc từ .env
+                .authorizationUri("https://github.com/login/oauth/authorize")
+                .tokenUri("https://github.com/login/oauth/access_token")
+                .userInfoUri("https://api.github.com/user")
+                .redirectUri("https://www.laptopaz.id.vn/login/oauth2/code/github")
+                .scope("user:email")
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .userNameAttributeName("login")
+                .build();
+
+        return new InMemoryClientRegistrationRepository(Arrays.asList(googleClient, facebookClient, githubClient));
     }
 
     @Bean
