7b5c6ba55d8e7f8ca7ad4f843e0f7fd9096a10f8
mrcodewalker <mr.codewalker@gmail.com>
2024-10-25 14:09:58 +0700

    KMA Legend

--- src/main/java/com/example/codewalker/kma/services/ICrawlDataService.java
+++
@@ -1,9 +0,0 @@
-package com.example.codewalker.kma.services;
-
-import org.springframework.http.ResponseEntity;
-
-import java.io.IOException;
-
-public interface ICrawlDataService {
-    ResponseEntity<?> login(String username, String password) throws IOException;
-}
